91ad36cffb499d2f2dc617725eba665cb7b943ef
ambush <83825719+ambush8832@users.noreply.github.com>
2023-05-01 01:51:52 +0500

    uranium releases fallout particles (very smoll) when destroyed improperly

--- lua/entities/ent_jack_gmod_ezuranium.lua
+++ lua/entities/ent_jack_gmod_ezuranium.lua
@@ -1,55 +1,39 @@
-﻿-- Jackarunda 2021
+-- Jackarunda 2021
 AddCSLuaFile()
 ENT.Base = "ent_jack_gmod_ezresource"
-ENT.PrintName = "EZ Uranium Ingot"
+ENT.PrintName = "EZ Uranium Ore"
 ENT.Category = "JMod - EZ Resources"
-ENT.IconOverride = "materials/ez_resource_icons/uranium.png"
+ENT.IconOverride = "materials/ez_resource_icons/uranium ore.png"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
 ---
-ENT.EZsupplies = JMod.EZ_RESOURCE_TYPES.URANIUM
-ENT.JModPreferredCarryAngles = Angle(180, 90, -90)
-ENT.Model = "models/jmod/resources/ingot001.mdl"
-ENT.Material = "models/props_mining/ingot_jack_uranium"
-ENT.Color = Color(50, 55, 50)
+ENT.EZsupplies = JMod.EZ_RESOURCE_TYPES.URANIUMORE
+ENT.JModPreferredCarryAngles = Angle(0, 0, 0)
+ENT.Model = "models/hunter/blocks/cube05x05x05.mdl"
+ENT.Material = "models/mat_jack_gmod_uraniumore"
 ENT.ModelScale = 1
-ENT.Mass = 100
-ENT.ImpactNoise1 = "SolidMetal.ImpactHard"
+ENT.Mass = 50
+ENT.ImpactNoise1 = "Rock.ImpactHard"
 ENT.DamageThreshold = 120
-ENT.BreakNoise = "SolidMetal.ImpactHard"
+ENT.BreakNoise = "Boulder.ImpactHard"
 
----
 if SERVER then
-	function ENT:UseEffect(pos, ent)
-	end
+	function ENT:UseEffect(pos, ent, destructive)
+		if destructive and not self.Sploomd then
+			self.Sploomd = true
+			local Owner, Count = self.EZowner, self:GetResource() / 10
 
-	-- it's metal
-	function ENT:CustomThink()
-		if math.random(1, 200) <= self:GetResource() then
-			local Ent = ents.Create("ent_jack_gmod_ezfalloutparticle")
-			Ent:SetPos(self:GetPos() + Vector(0, 0, 10))
-			Ent.EZowner = self.EZowner
-			Ent.LifeTime = 15
-			Ent.DmgAmt = 1
-			Ent.Range = 500
-			Ent.DragMult = .3
-			Ent:Spawn()
-			Ent:Activate()
-			Ent:SetVelocity(self:GetVelocity())
+			timer.Simple(.5, function()
+				for k = 1, JMod.Config.Particles.NuclearRadiationMult * Count / 2 do
+					local Gas = ents.Create("ent_jack_gmod_ezfalloutparticle")
+					Gas.Range = 500
+					Gas:SetPos(pos)
+					JMod.SetEZowner(Gas, Owner or game.GetWorld())
+					Gas:Spawn()
+					Gas:Activate()
+					Gas:GetPhysicsObject():SetVelocity(Vector(0, 0, 50 * JMod.Config.Particles.NuclearRadiationMult))
+				end
+			end)
 		end
-
-		self:NextThink(CurTime() + math.Rand(10, 20))
-
-		return true
 	end
-elseif CLIENT then
-	function ENT:Draw()
-		self:DrawModel()
-
-		JMod.HoloGraphicDisplay(self, Vector(0, -3, 4.9), Angle(0, 0, 0), .025, 300, function()
-			JMod.StandardResourceDisplay(JMod.EZ_RESOURCE_TYPES.URANIUM, self:GetResource(), nil, 0, 0, 200, false)
-		end)
-	end
-
-	language.Add(ENT.ClassName, ENT.PrintName)
 end
